72aa792086b5a92b3a4e9d58418933c85e65855e
Jaldazabal3 <jaldazes7@alumnes.ub.edu>
2016-10-11 16:30:07 +0200

    Update ExempleClasse2.java

--- exempleClasse2/src/exempleclasse2/ExempleClasse2.java
+++ exempleClasse2/src/exempleclasse2/ExempleClasse2.java
@@ -16,7 +16,7 @@ public class ExempleClasse2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HelloWorld");
+        System.out.println("HelloPeople");
     }
     
 }
