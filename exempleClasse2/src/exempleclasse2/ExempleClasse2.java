2d7de0c987b30416a4f67c2fe60c2f02d10f23f7
Jaldazabal3 <>
2016-10-11 16:17:32 +0200

    Another commit

---
+++ exempleClasse2/src/exempleclasse2/ExempleClasse2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exempleclasse2;
+
+/**
+ *
+ * @author jaldazabal
+ */
+public class ExempleClasse2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HelloWorld");
+    }
+    
+}
